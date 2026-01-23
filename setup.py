8885fc59fe9376653bad55a702d781e2090adba1
BELBINBENORM <85445374+BELBINBENORM@users.noreply.github.com>
2026-01-23 12:20:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name="file_select_regression",
+    name="select_regression",
     version="0.1",
-    py_modules=["file_select_regression"], # This points to your .py filename
+    py_modules=["select_regression"], # This points to your .py filename
     install_requires=[
         'pandas',
         'scikit-learn',
