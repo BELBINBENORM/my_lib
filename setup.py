d0daf9112b440cc87eb0f60ff92f3990bb815c37
BELBINBENORM <85445374+BELBINBENORM@users.noreply.github.com>
2026-01-23 23:38:29 +0530

    Add psutil and matplotlib to install_requires

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from setuptools import setup
 setup(
     name="select_regression",
     version="0.1",
-    py_modules=["select_regression"], # This points to your .py filename
+    py_modules=["select_regression"], 
     install_requires=[
         'pandas',
         'scikit-learn',
         'joblib',
+        'psutil',      
+        'matplotlib',  
     ],
 )
